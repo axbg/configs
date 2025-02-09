38889afc4a9664773132dc80f33a1adfe2bc32cf
axbg <bisagalexstefan@gmail.com>
2025-02-09 11:51:07 +0200

    Update linux/debian.i3/.bash_aliases and connect-monitor.sh

--- linux/debian.i3/.bash_aliases
+++ linux/debian.i3/.bash_aliases
@@ -1,1 +1,8 @@
 alias dev="cd ~/Dev"
+alias v="nvim ."
+
+alias bee="kitty +kitten ssh 192.168.0.112"
+
+alias vpn-up="wg-quick up wg0"
+alias vpn-down="wg-quick down wg0"
+
--- linux/debian.i3/scripts/connect-monitor.sh
+++ linux/debian.i3/scripts/connect-monitor.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if xrandr | grep -q "^DP-1 connected"; then
-    /home/axbg/.screenlayout/monitor-c1.sh
+  /home/axbg/.screenlayout/monitor-c1.sh
+  feh --bg-scale /home/axbg/Documents/wp.jpg
 elif xrandr | grep -q "^DP-2 connected"; then
-    /home/axbg/.screenlayout/monitor-c2.sh
+  /home/axbg/.screenlayout/monitor-c2.sh
+  feh --bg-scale /home/axbg/Documents/wp.jpg
 else
-    echo "Neither DP-1 nor DP-2 is connected."
+  echo "Neither DP-1 nor DP-2 is connected."
 fi
+
