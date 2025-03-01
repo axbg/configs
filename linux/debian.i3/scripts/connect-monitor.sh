a450f7aace9a40a828a8d7411a45a11eba917a04
axbg <bisagalexstefan@gmail.com>
2025-03-01 15:43:35 +0200

    Update debian.i3/scripts/connect-monitor.sh

--- linux/debian.i3/scripts/connect-monitor.sh
+++ linux/debian.i3/scripts/connect-monitor.sh
@@ -3,12 +3,10 @@
 if xrandr | grep -q "^DP-1 connected"; then
   /home/axbg/.screenlayout/monitor-c1.sh
   sleep 1 
-  feh --bg-scale /home/axbg/Documents/wp.jpg
 elif xrandr | grep -q "^DP-2 connected"; then
   /home/axbg/.screenlayout/monitor-c2.sh
   sleep 1
-  feh --bg-scale /home/axbg/Documents/wp.jpg
-else
-  echo "Neither DP-1 nor DP-2 is connected."
 fi
+
+feh --bg-scale /home/axbg/Documents/wp.jpg
 
